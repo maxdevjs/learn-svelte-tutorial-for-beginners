c96524e9859ccf463fe189ecbdd6b551bdf901a7
maxdevjs <max.devjs@gmail.com>
2020-04-13 17:55:44 -0300

    Add .modal style (lesson-10)

--- myproject/src/Modal.svelte
+++ myproject/src/Modal.svelte
@@ -22,4 +22,12 @@
     background: rgba(0, 0, 0, 0.8);
   }
 
+  .modal {
+    padding: 10px;
+    border-radius: 10px;
+    max-width: 400px;
+    margin: 10% auto;
+    text-align: center;
+    background: white;
+  }
 </style>
