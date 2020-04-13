b4e1330b4446cb5d5a2058162d70f4e64d02a55e
maxdevjs <max.devjs@gmail.com>
2020-04-13 17:52:28 -0300

    Add local .backdrop style (lesson-10)

--- myproject/src/Modal.svelte
+++ myproject/src/Modal.svelte
@@ -3,12 +3,16 @@
 </script>
 
 {#if showModal}
-  <div class="backdrop">
     <div class="modal">
-      <p>Sign up for offers</p>
     </div>
   </div>
 {/if}
 <style>
+  .backdrop {
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    background: rgba(0, 0, 0, 0.8);
+  }
 
 </style>
