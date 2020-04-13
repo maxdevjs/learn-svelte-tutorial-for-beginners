d36fc6c7993906dfed4023895ead2c8358a7200d
maxdevjs <max.devjs@gmail.com>
2020-04-13 19:06:57 -0300

    Implement Modal event modifier (lesson-13)

--- myproject/src/Modal.svelte
+++ myproject/src/Modal.svelte
@@ -1,13 +1,20 @@
 <script>
+  // Event Modfiers:
+  // once - makes sure the event can only fire once (removes handler)
+  // preventDefault - prevent the default action (run e.preventDefault())
+  // self - only fires the event if the clicked element is the target
+  // ...
+
   export let message = 'deafult value'; // passed as a prop
   export let showModal = false;
   export let isPromo = false;
 </script>
 
 {#if showModal}
-  <div class="backdrop" class:promo={isPromo} on:click>
+  <div class="backdrop" class:promo={isPromo} on:click|self>
     <div class="modal">
-      <p>{message}</p>
+      <a href="http://localhost:5000/promo"><p>{message}</p></a>
+      <!-- <p>{message}</p> -->
       <!-- {#if isPromo}
         <p>There is our promo</p>
       {:else}
