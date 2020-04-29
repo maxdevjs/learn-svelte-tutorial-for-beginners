6960a519f172e72cf551f80ea94bff0f5fe7c7ba
maxdevjs <max.devjs@gmail.com>
2020-04-29 02:02:02 -0300

    Import create poll form.

--- ninjapolls/src/App.svelte
+++ ninjapolls/src/App.svelte
@@ -1,6 +1,7 @@
 <script>
 	import Header from './components/Header.svelte';
 	import Footer from './components/Footer.svelte';
+	import CreatePollForm from './components/CreatePollForm.svelte'
 	import Tabs from './shared/Tabs.svelte';
 
 	// tabs
