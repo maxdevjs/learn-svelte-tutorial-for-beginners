5a3e496b697834e9f03d1a756b8c7eb3e83512a4
maxdevjs <max.devjs@gmail.com>
2020-04-29 02:04:00 -0300

    Create create poll form.

---
+++ ninjapolls/src/components/CreatePollForm.svelte
@@ -0,0 +1,22 @@
+<script>
+
+</script>
+
+<form>
+  <div class="form-field">
+    <label for="question">Poll Question:</label>
+    <input type="text" id="question">
+  </div>
+  <div class="form-field">
+    <label for="answer-a">Answer A:</label>
+    <input type="text" id="answer-a">
+  </div>
+  <div class="form-field">
+    <label for="answer-b">Answer B:</label>
+    <input type="text" id="answer-b">
+  </div>
+</form>
+
+<style>
+
+</style>
