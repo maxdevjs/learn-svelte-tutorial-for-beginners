617925e8011dc936a865c8f4f89f7be815353ee3
maxdevjs <max.devjs@gmail.com>
2020-04-28 23:48:30 -0300

    Update App.svelte

--- ninjapolls/src/App.svelte
+++ ninjapolls/src/App.svelte
@@ -1,10 +1,17 @@
 <script>
+	import Header from './components/Header.svelte';
+	import Footer from './components/Footer.svelte';
 </script>
 
+<Header />
 <main>
-	<h1>Hello, Ninjas!</h1>
+	<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos at necessitatibus consequuntur praesentium asperiores consequatur nam corrupti. Dolorum dignissimos iusto, repudiandae qui culpa eos quod, nulla eligendi id sed odio.</p>
 </main>
+<Footer />
 
 <style>
-
+	main {
+		max-width: 960px;
+		margin: 40px auto;
+	}
 </style>
