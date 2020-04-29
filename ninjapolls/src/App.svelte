bf328b92e5df3dddc6e4bf108e22ed9ac1cac119
maxdevjs <max.devjs@gmail.com>
2020-04-29 01:23:22 -0300

    Import Tabs.

--- ninjapolls/src/App.svelte
+++ ninjapolls/src/App.svelte
@@ -1,6 +1,7 @@
 <script>
 	import Header from './components/Header.svelte';
 	import Footer from './components/Footer.svelte';
+	import Tabs from './shared/Tabs.svelte';
 
 	// tabs
 	let items = ['Current Polls', 'Add New Poll'];
