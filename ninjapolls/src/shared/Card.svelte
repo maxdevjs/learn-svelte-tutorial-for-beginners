02b57decb5088da4dd551f2c2685fb63a64b7b69
maxdevjs <max.devjs@gmail.com>
2020-04-30 02:16:53 -0300

    Implement card.

---
+++ ninjapolls/src/shared/Card.svelte
@@ -0,0 +1,13 @@
+<div class="card">
+  <slot></slot>
+</div>
+
+<style>
+  .card {
+    background: white;
+    padding: 20px;
+    border: 2px solid #d91b42;
+    border-radius: 6px;
+    box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+  }
+</style>
