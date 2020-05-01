a1d9f074cc2ad8dcad67dddc6cf354b47927b428
maxdevjs <max.devjs@gmail.com>
2020-05-01 01:10:33 -0300

    Check life cycle hooks.

--- ninjapolls/src/components/PollList.svelte
+++ ninjapolls/src/components/PollList.svelte
@@ -1,16 +1,28 @@
 <script>
+  // import { onMount, onDestroy } from 'svelte';
   import PollStore from '../stores/PollStore.js';
   import PollDetails from './PollDetails.svelte';
-  
-  export let polls = [];
 
-  PollStore.subscribe(data => {
-    polls = data;
-  });
+  // export let polls = [];
+
+  // const unsub = PollStore.subscribe(data => {
+  //   polls = data;
+  // });
+
+  // onMount(() => {
+  //   // data from db?
+  //   console.log('component mounted');
+  // });
+
+  // onDestroy(() => {
+  //   // unsub from store
+  //   unsub();
+  //   console.log('component destroyed');
+  // });
 </script>
 
 <div class="poll-list">
-  {#each polls as poll (poll.id)}
+  {#each $PollStore as poll (poll.id)}
     <PollDetails {poll} on:vote />
 	{/each}
 </div>
